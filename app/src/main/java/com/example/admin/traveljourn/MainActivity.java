35f12563e27abdad51c81724c8d0e8ac7d0e6584
Pearly <potegopearl@gmail.com>
2017-02-06 15:23:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/admin/traveljourn/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.traveljourn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
