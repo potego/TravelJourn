70bb27ea61c8994a4c6fcc486eef102bb2de7367
Pearly <potegopearl@gmail.com>
2017-02-14 13:59:57 +0200

    Changes

--- app/src/main/java/com/example/admin/traveljourn/MainActivity.java
+++ app/src/main/java/com/example/admin/traveljourn/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.admin.traveljourn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button avatar = (Button) findViewById(R.id.reg_id);
+
+        avatar.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                //Intent to the profile activity
+                Intent profileIntent = new Intent(MainActivity.this, landingPage.class);
+                startActivity(profileIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/admin/traveljourn/landingPage.java
+++ app/src/main/java/com/example/admin/traveljourn/landingPage.java
@@ -1,7 +1,11 @@
 package com.example.admin.traveljourn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class landingPage extends AppCompatActivity {
 
@@ -9,5 +13,51 @@ public class landingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        ImageView avatar = (ImageView)findViewById(R.id.avatar_id);
+
+        avatar.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                //Intent to the profile activity
+                Intent profileIntent = new Intent(landingPage.this, Profile.class);
+                startActivity(profileIntent);
+            }
+        });
+
+        ImageView new_note = (ImageView)findViewById(R.id.new_note_id);
+
+            new_note.setOnClickListener(new View.OnClickListener(){
+
+                public void onClick(View view){
+                    //Intent to the profile activity
+                    Intent new_noteIntent = new Intent(landingPage.this, New_note.class);
+                    startActivity(new_noteIntent);
+                }
+            });
+
+        ImageView chats = (ImageView)findViewById(R.id.chats_id);
+
+            chats.setOnClickListener(new View.OnClickListener(){
+
+                public void onClick(View view){
+                    //Intent to the profile activity
+                    Intent chatsIntent = new Intent(landingPage.this, Chats.class);
+                    startActivity(chatsIntent);
+                }
+        });
+
+        Button save_me = (Button)findViewById(R.id.save_id);
+
+            save_me.setOnClickListener(new View.OnClickListener(){
+
+                public void onClick(View view){
+                    //Intent to the profile activity
+                    Intent sosIntent = new Intent(landingPage.this, MainActivity.class);
+                    startActivity(sosIntent);
+                }
+        });
+
+
     }
 }
